f32b1d390fd3673e38fba4ba29f4df2b5b0ba032
Runtym <redfilaruntime@gmail.com>
2018-10-02 13:23:19 +0900

    ict1 erp init

---
+++ src/main/java/com/ict/erp/Test.java
@@ -0,0 +1,35 @@
+package com.ict.erp;
+
+class Init{
+	
+	
+}
+public class Test {
+	
+	private Init i;
+	
+	public Test() {
+		
+	}
+	
+	public Init getInit() {
+		i = new Init();
+		return i;
+	}
+	public void close() {
+		this.i = null;
+	}
+	
+	public void printI() {
+		System.out.println(i);
+	}
+	public static void main(String[] args) {
+		
+		Test t = new Test();
+		Init i = t.getInit();
+		t.close();
+		i = null;
+		System.out.println(i);
+		t.printI();
+	}
+}
